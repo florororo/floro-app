05babfdfffc399a9d7d1aba0de3560da7230d856
Felipe Pattillo <37159447+fpattillo@users.noreply.github.com>
2026-01-13 20:54:20 -0300

    gitignore update, db config (#2)

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,8 +1,15 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+
 import { env } from "~/env";
 import { PrismaClient } from "../../generated/prisma";
 
+const pool = new Pool({ connectionString: env.DATABASE_URL });
+const adapter = new PrismaPg(pool);
+
 const createPrismaClient = () =>
   new PrismaClient({
+    adapter,
     log:
       env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
   });
